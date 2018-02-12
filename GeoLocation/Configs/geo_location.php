e1f0ebc0753e7c16cc264f815acaaae0626b4836
Fabian Widmann <fabian.widmann@uni-ulm.de>
2018-02-12 12:20:49 +0100

    changed structure

--- GeoLocation/Configs/geo_location.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'geo_location_table_name' => 'geo_location',
-    /**
-     * Defines the number of geo_location models that should be used to query the weather providers per trigger.
-     */
-
-];
